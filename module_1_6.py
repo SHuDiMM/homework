d6356703ba78248f466a62a1368425a59371f242
SHuDiMM <90505584+SHuDiMM@users.noreply.github.com>
2024-10-29 09:26:01 +0700

    29/10

--- module_1_6.py
+++ module_1_6.py
@@ -1,3 +1,17 @@
+from random import random, Random
+
 my_dict = {"Sergei":1992, "Gosha":1975, "Andrei":2001 }
-print(my_dict)
-print(my_dict.get("Sergei"),my_dict.get("Kuzma"))
+print("Dict:",my_dict)
+print("Existing value:",my_dict.get("Sergei"),"\n"+"Not existing value:", my_dict.get("Kuzma"))
+my_dict.update({'Dmitry':1990, 'Roman':1655})
+removed_element = my_dict.pop('Gosha')
+print("Deleted value:", removed_element)
+print("Modified dictionary:", my_dict)
+print()
+my_set = {2,2,2,0.7,0.9,3.4,3.4,0.7,'Monitor', 'Weekend', ("синий", "зеленый", "красный"),'Monitor' }
+print("Set:", my_set)
+my_set.add("Mouse")
+my_set.add(4.8)
+my_set.discard(Random)
+print("Modified set:", my_set)
+
