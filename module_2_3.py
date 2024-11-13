6b9bd16daa287ede284de7dfa32f580d4ec6ace9
SHuDiMM <90505584+SHuDiMM@users.noreply.github.com>
2024-11-13 11:02:11 +0700

    Module_2.4_13.11.2024

--- module_2_3.py
+++ module_2_3.py
@@ -1,5 +1,9 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index_of_element = int(0)
+print('Исходные данные:')
+print('my_list =', my_list, '''
+''')
+print('Вывод на консоль:')
 while len(my_list) > (index_of_element + 1):
     if my_list[index_of_element] > 0:
         print(my_list[index_of_element])
