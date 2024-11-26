d6302afe5e9b0aa0835bc1c58b3cc0fa5e7e6a3e
SHuDiMM <90505584+SHuDiMM@users.noreply.github.com>
2024-11-26 23:30:18 +0700

    Module_3.5_26.11.2024

--- module_3_5.py
+++ module_3_5.py
@@ -2,9 +2,12 @@ def get_multiplied_digits(number=int()):
     str_number = str(number)
     first = int(str_number[0])
     if len(str_number) == 1:
-        return first
+        if first == 0:
+            return 1
+        else:
+            return first
     else:
         return first * get_multiplied_digits(int(str_number[1:]))
 
-result_1 = get_multiplied_digits(40203)
+result_1 = get_multiplied_digits(402030)
 print(result_1)
